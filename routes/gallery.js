4da84dfb267d4a397ecddb178508de3091862ebc
Steven Inouye <steveninouye@msn.com>
2018-04-16 04:02:46 -1000

    working on ADD NEW PICTURE and EDIT PICTURE FORMS.... home page and picture details rendering

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,26 +1,49 @@
 const router = require('express').Router();
+const Users = require('../db/models/Users');
+const Pictures = require('../db/models/Pictures');
 
-router.get('/:id', (req, res) => {
-  //see a single gallery photo
-  res.send('testing');
-});
+const combineAttributes = fetchedData => {
+  return fetchedData.models.map(e =>
+    Object.assign(e.attributes, e.relations.author.attributes)
+  );
+};
 
-router.get('/new', (req, res) => {
-  //see a new photo form
-  //fields author, link, description
-});
+const findPictureByID = (array, id) => {
+  return array.filter(e => e.picture_id === id)[0];
+};
 
 router.post('/', (req, res) => {
   //create a new gallery photo
 });
 
-router.get('/:id/edit', (req, res) => {
-  //see a form to edit a gallery photo identified by the :id
-  //form fields are author, link, description
+router.get('/new', (req, res) => {
+  res.render('newPicture');
+});
+
+router.get('/:id', (req, res) => {
+  Pictures.fetchAll({ withRelated: ['author'] })
+    .then(result => {
+      const allPictures = combineAttributes(result);
+      const selectedPicture = findPictureByID(
+        allPictures,
+        Number(req.params.id)
+      );
+      res.render('picture_detail', { selectedPicture, allPictures });
+      //   res.render('index', { result });
+    })
+    .catch(err => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   //updates a single gallergy photo identified by the :id param
+});
+
+router.get('/:id/edit', (req, res) => {
+  //see a form to edit a gallery photo identified by the :id
+  //form fields are author, link, description
 });
 
 router.delete('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -4,26 +4,33 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const Users = require('./db/models/Users');
 const Pictures = require('./db/models/Pictures');
 
 app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 app.engine('hbs', hbs.__express);
 hbs.registerPartials(__dirname + '/views/partials');
-hbs.registerHelper('capitalFirstLetter', str => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+hbs.registerHelper(
+  'capitalFirstLetter',
+  str => str.charAt(0).toUpperCase() + str.slice(1)
+);
+hbs.registerHelper('getUsername', str => str.split('@')[0]);
+hbs.registerHelper('shortenURL', str => {
+  str = str.length > 50 ? str.slice(0, 50) + '...' : str;
+  return str;
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/gallery', require('./routes/gallery'));
-app.use(express.static(__dirname + '/public'));
 app.use(require('express-method-override')());
 
 app.get('/', (req, res) => {
   Pictures.fetchAll({ withRelated: ['author'] })
     .then(result => {
-      console.log(result.models[0].attributes);
-      res.render('index', { data: result.models });
+      const allPictures = result.models.map(e =>
+        Object.assign(e.attributes, e.relations.author.attributes)
+      );
+      res.render('index', { allPictures });
       //   res.render('index', { result });
     })
     .catch(err => {
